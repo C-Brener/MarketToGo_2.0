9cea9c15ed8feb37228402fdc615ece9167f1253
Caíque Brener <65732426+C-Brener@users.noreply.github.com>
2023-12-26 19:53:16 -0300

    test: ensure DbAddAccount Throws if addAccountRepository throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -94,4 +94,21 @@ describe('DbAddAccount UseCase', () => {
       phoneNumber: 'valid_number'
     })
   })
+
+  test('Should throw if AddAccountRepository throws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+
+    jest.spyOn(addAccountRepositoryStub, 'add').mockResolvedValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password',
+      phoneNumber: 'valid_number'
+    }
+
+    const promiseAccount = sut.add(accountData)
+
+    await expect(promiseAccount).rejects.toThrow()
+  })
 })
