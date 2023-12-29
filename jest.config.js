e1d16e6b08db29042cafbc73eb5a0e8792edcb51
Caíque Brener <65732426+C-Brener@users.noreply.github.com>
2023-12-28 23:20:09 -0300

    chore: add jest configs for mongodb

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  preset: '@shelf/jest-mongodb',
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts': 'ts-jest'
-  },
-  preset: '@shelf/jest-mongodb'
+  }
 }
