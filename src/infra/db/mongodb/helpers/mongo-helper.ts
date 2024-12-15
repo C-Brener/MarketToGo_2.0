dcd24d50d92422d61a0b3757624b0831af24778e
Caíque Brener <65732426+C-Brener@users.noreply.github.com>
2024-12-15 08:36:58 -0300

    feat: ensure that the database tries to reconnect when the connection is lost

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,7 +1,10 @@
 import { type Collection, MongoClient } from 'mongodb'
 export const MongoHelper = {
   client: MongoClient,
+  uri: null as string | null,
+
   async connect (uri: string): Promise<void> {
+    this.uri = uri
     this.client = await MongoClient.connect(uri)
   },
   async disconnect (): Promise<void> {
@@ -9,8 +12,13 @@ export const MongoHelper = {
     this.client = null
   },
 
-  getCollection (name: string): Collection {
+  async getCollection (name: string): Promise<Collection> {
+    if (!this.isConnected()) await this.connect(this.uri)
     return this.client.db().collection(name)
+  },
+
+  isConnected (): boolean {
+    return !!this.client && this.client.topology?.isConnected()
   },
 
   map: (collection: any): any => {
