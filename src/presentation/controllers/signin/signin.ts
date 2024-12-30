82143709d6ab007429e61decaefe58fdc25e666b
Caíque Brener <65732426+C-Brener@users.noreply.github.com>
2024-12-30 09:48:25 -0300

    feat: add EmailValidator to SignInController and validate email in handle method

--- src/presentation/controllers/signin/signin.ts
+++ src/presentation/controllers/signin/signin.ts
@@ -1,8 +1,15 @@
 import { MissingParamError } from '../../errors'
 import { badRequest, ok } from '../../helpers/http-helper'
 import type { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import type { EmailValidator } from '../signup/signup-protocols'
 
 export class SignInController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.body.email) {
       return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
@@ -10,6 +17,8 @@ export class SignInController implements Controller {
     if (!httpRequest.body.password) {
       return new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
     }
+
+    this.emailValidator.isValid(httpRequest.body.email as string)
 
     return new Promise(resolve => resolve(ok('')))
   }
